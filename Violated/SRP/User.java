207fbdc01374851c4efe8f1ecf71d8d1f651553b
harwindersingh0 <152362683+harwindersingh0@users.noreply.github.com>
2024-03-22 20:48:58 -0600

    add all

---
+++ Violated/SRP/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String name;
+    private String email;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public void sendEmail(String message) {
+
+    }
+}
